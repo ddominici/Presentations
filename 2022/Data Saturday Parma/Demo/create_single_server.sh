d0a8dbefac8b292da9252c622d5c64017b4b03e8
Danilo <ddominici@gmail.com>
2023-02-27 10:18:38 +0100

    Added more presentations

---
+++ 2022/Data Saturday Parma/Demo/create_single_server.sh
@@ -0,0 +1,8 @@
+az login
+
+az account set --subscription "4708c8e5-b317-4edd-bc2b-3c5a6def23ed"
+
+az postgres server create -l westeurope -g "RG-DD-POSTGRESQL" -n pgdd02 -u danilo -p "P@ssw0rd!" \
+    --sku-name B_Gen5_1 --ssl-enforcement Enabled --minimal-tls-version TLS1_0 --public-network-access Enabled \
+    --backup-retention 10 --geo-redundant-backup Disabled --storage-size 51200 \
+    --tags "location=Pordenone" --version 11
