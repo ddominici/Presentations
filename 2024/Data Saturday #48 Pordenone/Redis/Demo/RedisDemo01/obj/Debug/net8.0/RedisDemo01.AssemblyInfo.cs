11e5260c45ce675f875827d377bd049a94a29208
ddominici <ddominici@hotmail.com>
2024-12-16 21:44:09 +0100

    XmasDev 2024

--- 2024/Data Saturday #48 Pordenone/Redis/Demo/RedisDemo01/obj/Debug/net8.0/RedisDemo01.AssemblyInfo.cs
+++ 2024/Data Saturday #48 Pordenone/Redis/Demo/RedisDemo01/obj/Debug/net8.0/RedisDemo01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedisDemo01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21a940b8962ba34b5de97b3a8d29526d9e78a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedisDemo01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedisDemo01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 2024/XmasDev/Demo/Setup/README.md
@@ -0,0 +1,11 @@
+# Setup della progetto SantaDelivery
+
+## Prerequisiti
+
+- Azure CLI installato
+- Powershell 5+
+
+## Istruzioni
+
+1) Eseguire lo script Powershell setupAzure.ps1
+2) TBD
